5d3d6af67c6e136ae32d0cee71d9e9fbf87f1bc8
Mykola Zorennyi <mykola.zorennyi@overdose.digital>
2021-12-02 17:20:47 +0200

    update config https://www.wrike.com/open.htm?id=804095530

--- Model/Config/Constants.php
+++ Model/Config/Constants.php
@@ -4,5 +4,5 @@ namespace Overdose\Core\Model\Config;
 
 class Constants
 {
-    const OVERDOSE_CLOBAL_CONFIG_GOOGLE_API_KEY = 'od_general_config/api_keys/google_maps';
+    const OVERDOSE_GLOBAL_CONFIG_GOOGLE_API_KEY = 'od_general_config/api_keys/google_maps';
 }
