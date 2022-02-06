afad30b5a07314c7ea3fcd7faad7e7f0b445dce9
Volodumyr Novitsky <orthener.ua@gmail.com>
2022-02-06 15:49:12 +0200

    support php7.3. Update readme

--- Model/Config/EnvironmentConfig.php
+++ Model/Config/EnvironmentConfig.php
@@ -10,7 +10,7 @@ class EnvironmentConfig
     /**
      * @var ScopeConfigInterface
      */
-    private ScopeConfigInterface $scopeConfig;
+    private $scopeConfig;
 
     /**
      * @param ScopeConfigInterface $scopeConfig
